aea1e735873ba5dbb251807a749ea8ce1b1f6d82
kgoli1 <kgolimatlala39@gmail.com>
2023-06-16 00:03:40 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0 success, non zero fail
+ */
+int main(void)
+{
+	int i, k = '0', j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+
