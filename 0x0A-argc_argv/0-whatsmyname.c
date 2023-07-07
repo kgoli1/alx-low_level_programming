0c1c80bce9f35e4aa262ff306823a4f5d85dbc8e
kgoli1 <kgolimatlala39@gmail.com>
2023-07-07 13:41:52 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
+
 /**
- * main - function thats outputs the string
- * @argc: integer
- * @argv: character
- * Return: 0
- */
-
-int main(int __attribute__((__unused__)) argc, char *argv[])
-
+* main - prints its name, followed by a new line.
+* @argc: argument count
+* @argv: arguments
+*
+* Return: 0
+*/
+int main(int argc, char **argv)
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+(void)argc;
 
-	return (0);
+
+printf("%s\n", argv[0]);
+return (0);
 }
 
