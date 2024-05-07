9adfe991abd034971a726b6d7f4357c74d1d7d0b
kgoli1 <kgolimatlala39@gmail.com>
2024-05-07 15:45:32 +0000

    create a dynamic/shared library

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,5 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*_MAIN_H_ */
-
+#endif /*_MAIN_H_*/
